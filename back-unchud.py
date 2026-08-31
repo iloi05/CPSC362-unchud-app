599badb046ab05fac01a5b23e5922f37681b6b8e
root <root@DESKTOP-8969P1M.localdomain>
2026-08-31 15:08:40 -0700

    started notification function

--- back-unchud.py
+++ back-unchud.py
@@ -1,8 +1,30 @@
 # This file holds the back-end code for Unchud
 
-from datetime import datetime
+from datetime import datetime, timezone
 
+class Assignment:
+    # variables for all functions
+    def __init__(self):
+        self.assignment = {}
+        self.moneyCounter = 0
 
-def add_assigment():
-    assignment = input("Assignment name: ")
-    
+    # helper functions to check date and time
+    def check_date(self, date):
+        try:
+            datetime.strptime(date, "%Y-%m-%d")
+            return True
+        except ValueError:
+            return False
+    def check_time(self, time):
+        try:
+            datetime.strptime(time, "%H:%M %p")
+            return True
+        except ValueError:
+            return False
+
+    # main functions for the program
+    def add_assignment(self):
+        assignment = input("Assignment name: ")
+
+    def notification(self, current_time, current_date):
+        
