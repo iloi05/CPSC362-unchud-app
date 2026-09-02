552d0fcb2585276a54cf0a2330688cf7fb0d1ba2
root <root@DESKTOP-8969P1M.localdomain>
2026-09-02 12:30:19 -0700

    mark function half done

--- back-unchud.py
+++ back-unchud.py
@@ -26,5 +26,37 @@ class Assignment:
     def add_assignment(self):
         assignment = input("Assignment name: ")
 
-    def notification(self, current_time, current_date):
+    def mark_assignment(self):
+        current = datetime.now()
+        due_date = input("When is this assignment due? (YYYY-MM-DD): ")
+        if not self.check_date(due_date):
+            print("Invalid date format. Please use YYYY-MM-DD.")
+            return
+        due_time = input("What time is this assignment due? (HH:MM AM/PM): ")
+        if not self.check_time(due_time):
+            print("Invalid time format. Please use HH:MM AM/PM.")
+            return
+        class_name = input("What class is this assignment for? ")
+        assignment_name = input("What is the name of this assignment? ")
+
+        if due_date in self.assignment:
+            if class_name in self.assignment[due_date]:
+                if assignment_name in self.assignment[due_date][class_name]:
+                    self.assignment[due_date][due_time][class_name][assignment_name]["completed"] = True
+                    print(f"Congrats! You've finished {assignment_name} for {class_name}! :D")
+                    if self.assignment[due_date][due_time] < current:
+                        self.moneyCounter += 50
+                    elif self.assignment[due_date][due_time] > current:
+                        self.moneyCounter += 25
+                    elif self.assignment[due_date][due_time] == current:
+                        self.moneyCounter += 10
+                    print(f"You now have ${self.moneyCounter} in your account!")
+                    self.assignment[due_date][due_time][class_name][assignment_name]["completed"].remove()
+                
+
+
+
+
+
+        
         
