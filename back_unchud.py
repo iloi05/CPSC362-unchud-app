a5707511c8e5ae2af2007d93ca33a541e036150c
root <iloi05@csu.fullerton.edu>
2026-09-11 23:33:06 -0700

    mark_assignment function modified to fit API

--- back_unchud.py
+++ back_unchud.py
@@ -79,84 +79,113 @@ class Assignment:
             return {"sucess": False, 
                     "message": "Assignment already exists"}
 
+
+
+    def mark_assignment(self, due_date, due_time, class_name, assignment_name):
+        if class_name not in self.assignment:
+            return {"success": False, "message": "Class not found."}
+
+        if not self.check_date(due_date):
+                    return {"success": False, 
+                            "message: ": "Invalid date format. Please use YYYY-MM-DD."}  
+        
+        if not self.check_time(due_time):
+                    return {"success": False, 
+                        "message": "Invalid time format. Please use HH:MM AM/PM."}
+
         
 
-        
-
-
-
-    def mark_assignment(self):
-        # may need to change the way the dictionary is formatted [class][duedate][time][assignment]
-        current = datetime.now()
-        dueDate = input("What is the due date of the assignment you want to mark? (YYYY-MM-DD): ")
-        # idiot proofing the user input for date and time
-        if self.check_date(dueDate):
-            if dueDate in self.assignment:
-                dueTime = input("What is the due time of the assignment you want to mark? (HH:MM AM/PM): ")
-                if self.check_time(dueTime):
-                    if dueTime in self.assignment[dueDate]:
-                        className = input("What class is the assignment for? ")
-                        if className in self.assignment[dueDate][dueTime]:
-                            assignmentName = input("What is the name of the assignment you want to mark? ")
-                            if assignmentName in self.assignment[dueDate][dueTime][className]:
-                                print(f"Congrats! You completed {assignmentName} for {className}!")
-                                # turning work in early or on time gives big money
-                                if current < dueDate and current < dueTime:
-                                    self.moneyCounter += 50
-                                    print(f"You've gained ${self.moneyCounter} for completing this assignment early!")
-                                elif current == dueDate and current < dueTime:
-                                    self.moneyCounter += 25
-                                    print(f"You've gained ${self.moneyCounter} for completing this assignment on time!")
-                                elif current == dueDate and current == dueTime:
-                                    self.moneyCounter += 15
-                                    print(f"You barely made it! You've gained ${self.moneyCounter} for completing this assignment just in time!")
-                                elif current == dueDate and current > dueTime:
-                                    # if user turns in work late, they lose money
-                                    self.moneyCounter -= 0.10
-                                    print(f"Dang... should've done this earlier. You've lost $0.10 for completing this assignment late.")
-                                    print(f"You remaining balance is ${self.moneyCounter}.")
-                                elif current > dueDate and current > dueTime:
-                                    self.moneyCounter -= 5
-                                    print(f"Aw man... you missed the deadline. You've lost $5.00 for completing this assignment late.")
-                                    print(f"You remaining balance is ${self.moneyCounter}.")
-                                self.assignment[dueDate][dueTime][className].remove(assignmentName)
-                            else:
-                                print("Assignment not found.")
-                        else:
-                            print("Class not found.")
-                    else:
-                        print("Due time not found.")
-                else:
-                    print("Invalid time format. Please use HH:MM AM/PM.")
-                    return
-            else:
-                print("Due date not found.")
+        if assignment_name not in self.assignment[class_name][due_date][due_time]:
+            return {"success": False, "message": "Assignment not found."}
         else:
-            print("Invalid date format. Please use YYYY-MM-DD.")
-            return
-
-    def setNotif(self):
-        cName = input("What class is this assignment for? ")
-        aName = input("What is the name of the assignment you want to be notified about? ")
-        if cName not in self.assignment:
-            print("Class not found.")
-            return
+            success = f"Congrats! You completed {assignment_name} for {class_name}! You get money for not being a chud! :D"
+            self.assignment[class_name][due_date][due_time].remove(assignment_name)
+            if datetime.now() < datetime.strptime(due_date + " " + due_time, "%Y-%m-%d %I:%M %p"):
+                self.moneyCounter += 50
+                success += f" You've gained ${self.moneyCounter} for completing this assignment early!"
+            elif datetime.now() == datetime.strptime(due_date + " " + due_time, "%Y-%m-%d %I:%M %p"):
+                self.moneyCounter += 25
+                success += f" You've gained ${self.moneyCounter} for completing this assignment on time!"
+            elif datetime.now() > datetime.strptime(due_date + " " + due_time, "%Y-%m-%d %I:%M %p"):
+                self.moneyCounter -= 0.10
+                success += f" You've lost $0.10 for completing this assignment late. Your remaining balance is ${self.moneyCounter}."
+            else:
+                self.moneyCounter -= 5
+                success += f" You've lost $5.00 for completing this assignment late. Your remaining balance is ${self.moneyCounter}."
+            return {"success": True, "message": success}
         
-        for due_date in self.assignment[cName]:
-            for due_time in self.assignment[cName][due_date]:
-                for assignment in self.assignment[cName][due_date][due_time]:
-                    if aName == assignment:
-                        timePick = input("What time do you want to be notified daily? (HH:MM AM/PM): ")
-                        if self.check_time(timePick):
-                            notif = (f" STOP CHUDDING! You have {assignment} for {cName} due at {due_time} on {due_date}.")
-                            print(f"You will be notified daily at {timePick} about {assignment} for {cName}.")
-                            cTime = self.convert(timePick)
-                            schedule.every().day.at(cTime).do(notification.notify, title="Unchud Reminder", message=notif)
-                            return
-                        else:
-                            print("Invalid time format. Please use HH:MM AM/PM.")
-                            return
-        print("Assignment not found.")
+
+    #    # may need to change the way the dictionary is formatted [class][duedate][time][assignment]
+    #    current = datetime.now()
+    #    dueDate = input("What is the due date of the assignment you want to mark? (YYYY-MM-DD): ")
+    #    # idiot proofing the user input for date and time
+    #    if self.check_date(dueDate):
+    #        if dueDate in self.assignment:
+    #            dueTime = input("What is the due time of the assignment you want to mark? (HH:MM AM/PM): ")
+    #            if self.check_time(dueTime):
+    #                if dueTime in self.assignment[dueDate]:
+    #                    className = input("What class is the assignment for? ")
+    #                    if className in self.assignment[dueDate][dueTime]:
+    #                        assignmentName = input("What is the name of the assignment you want to mark? ")
+    #                        if assignmentName in self.assignment[dueDate][dueTime][className]:
+    #                            print(f"Congrats! You completed {assignmentName} for {className}!")
+    #                            # turning work in early or on time gives big money
+    #                            if current < dueDate and current < dueTime:
+    #                                self.moneyCounter += 50
+    #                                print(f"You've gained ${self.moneyCounter} for completing this assignment early!")
+    #                            elif current == dueDate and current < dueTime:
+    #                                self.moneyCounter += 25
+    #                                print(f"You've gained ${self.moneyCounter} for completing this assignment on time!")
+    #                            elif current == dueDate and current == dueTime:
+    #                                self.moneyCounter += 15
+    #                                print(f"You barely made it! You've gained ${self.moneyCounter} for completing this assignment just in time!")
+    #                            elif current == dueDate and current > dueTime:
+    #                                # if user turns in work late, they lose money
+    #                                self.moneyCounter -= 0.10
+    #                                print(f"Dang... should've done this earlier. You've lost $0.10 for completing this assignment late.")
+    #                                print(f"You remaining balance is ${self.moneyCounter}.")
+    #                            elif current > dueDate and current > dueTime:
+    #                                self.moneyCounter -= 5
+    #                                print(f"Aw man... you missed the deadline. You've lost $5.00 for completing this assignment late.")
+    #                                print(f"You remaining balance is ${self.moneyCounter}.")
+    #                            self.assignment[dueDate][dueTime][className].remove(assignmentName)
+    #                        else:
+    #                            print("Assignment not found.")
+    #                    else:
+    #                        print("Class not found.")
+    #                else:
+    #                    print("Due time not found.")
+    #            else:
+    #                print("Invalid time format. Please use HH:MM AM/PM.")
+    #                return
+    #        else:
+    #            print("Due date not found.")
+    #    else:
+    #        print("Invalid date format. Please use YYYY-MM-DD.")
+    #        return
+
+    #def setNotif(self):
+    #    cName = input("What class is this assignment for? ")
+    #    aName = input("What is the name of the assignment you want to be notified about? ")
+    #    if cName not in self.assignment:
+    #        print("Class not found.")
+    #        return
+    #    
+    #    for due_date in self.assignment[cName]:
+    #        for due_time in self.assignment[cName][due_date]:
+    #            for assignment in self.assignment[cName][due_date][due_time]:
+    #                if aName == assignment:
+    #                    timePick = input("What time do you want to be notified daily? (HH:MM AM/PM): ")
+    #                    if self.check_time(timePick):
+    #                        notif = (f" STOP CHUDDING! You have {assignment} for {cName} due at {due_time} on {due_date}.")
+    #                        print(f"You will be notified daily at {timePick} about {assignment} for {cName}.")
+    #                        cTime = self.convert(timePick)
+    #                        schedule.every().day.at(cTime).do(notification.notify, title="Unchud Reminder", message=notif)
+    #                        return
+    #                    else:
+    #                        print("Invalid time format. Please use HH:MM AM/PM.")
+    #                        return
+    #    print("Assignment not found.")
 
 
     
